3c6ac518c69a821448a298d2887e252b2550e405
kucheriavii <kucheriavii@gmail.com>
2023-09-16 21:44:22 +0300

    try to add basic scripts and styles

--- functions.php
+++ functions.php
@@ -1,1 +1,8 @@
-<?
+<?php
+ add_action('wp_enqueue_scripts', 'auto_script');
+ 
+ function auto_script(){
+    wp_enqueue_style('style-name', get_stylesheet_directory_uri());
+    wp_enqueue_script( 'script-name', get_template_directory_uri(), '/js/example.js', array(), '1.0.0', true);
+ }
+?>
--- header.php
+++ header.php
@@ -6,13 +6,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
+
+<?php wp_head(); ?>
+<!--   
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="css/reset.css">
   <link rel="stylesheet" href="css/slick.css">
-  <link rel="stylesheet" href="css/jquery.fancybox.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/style.css"> -->
 </head>
 
 <body>
